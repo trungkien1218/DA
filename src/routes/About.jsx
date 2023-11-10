dc8b0be656a24595ae9065c16378b36951fd5594
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2023-11-10 11:09:22 +0700

    Update About.jsx

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,8 +1,28 @@
+import { Carousel } from 'antd'
 import React from 'react'
-
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 const About = () => {
   return (
-    <div>About page</div>
+    <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </Carousel>
   )
 }
 
