a84dcf028ddb09f19347e63a8a3a1231a06a4dee
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2023-11-11 21:28:10 +0700

    ádasdasd
    ádasdasd

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,26 +1,29 @@
 import { Carousel } from 'antd'
 import React from 'react'
 const contentStyle = {
-  height: '160px',
+  height: '600px',
   color: '#fff',
   lineHeight: '160px',
   textAlign: 'center',
-  background: '#364d79',
+  
 };
 const About = () => {
   return (
     <Carousel autoplay>
     <div>
-      <h3 style={contentStyle}>1</h3>
+      <h3 style={contentStyle}><div className='pic1'></div></h3>
     </div>
     <div>
-      <h3 style={contentStyle}>2</h3>
+      <h3 style={contentStyle}><div className='pic2'></div></h3>
     </div>
     <div>
-      <h3 style={contentStyle}>3</h3>
+      <h3 style={contentStyle}><div className='pic3'></div></h3>
     </div>
     <div>
-      <h3 style={contentStyle}>4</h3>
+      <h3 style={contentStyle}><div className='pic4'></div></h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}><div className='pic5'></div></h3>
     </div>
   </Carousel>
   )
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,35 +1,64 @@
 import React from 'react'
+import { Carousel } from 'antd'
+const contentStyle = {
+  height: '600px',
 
+  lineHeight: '160px',
+  textAlign: 'center',
+
+};
 const Home = () => {
   return (
     <>
 
       <section className='sec1'>
-        <div className='BG-main'>
-          <h1
-            className='myshadow '
-            style={{
-              textAlign: 'center',
-              color: 'white',
-              paddingTop: '550px',
-              fontSize: '70px'
-            }}>Technology world Lam </h1>
-          <p className='myshadow ' style={{
-            textAlign: 'center',
-            color: 'white',
-            paddingTop: '20px',
-            fontSize: '25px'
-          }}>Always accompany you wherever you are</p>
-        </div>
+        <Carousel autoplay>
+          <div>
+            <h3 style={contentStyle}><div className='pic1'>
+              <p className='text-caro myshadow'>Pretty</p>
+              <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
+            </div></h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}><div className='pic2'><p className='text-caro myshadow'>Forever</p>
+
+            </div></h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}><div className='pic3'><p className='text-caro myshadow'>Sophisticated</p>
+              <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
+            </div></h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}><div className='pic4'><p className='text-caro myshadow'>Unique</p>
+              <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
+            </div></h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}><div className='pic5'><p className='text-caro myshadow'>particularistic</p></div></h3>
+          </div>
+        </Carousel>
+
+
       </section>
       <section className='sec2 container'>
         <div className='text2 '>
-          <span className='myshadow2 ' style={{fontSize:'40px', color: '#01455a'}}>Information technology</span> (English: Information Technology - IT), IT abbreviation, is a branch of engineering using computer and computer software for conversion, storage, protection, processing, transmission and collection Cross information.
+          <p className='myshadow2 ' style={{ fontSize: '40px', color: '#ff0597' }}>Flower</p> The flowers are meticulously knit meticulously. You can buy in a bouquet or individual
 
         </div>
         <div className="img2">
 
         </div>
+      </section>
+      <section className='sec2 container'>
+        <div className="img3">
+
+        </div>
+        <div className='text2 '>
+          <p className='myshadow2 ' style={{ fontSize: '40px', color: '#ff0597' }}>Animals</p> made sophisticated, lovely and beautifully suitable for everyone's tastes
+
+        </div>
+
       </section>
     </>
   )
