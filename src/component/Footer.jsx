122640f94e9b7908b1c7319a41bb6faee6463a6b
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2023-11-13 14:54:35 +0700

    13232

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { FacebookOutlined, InstagramOutlined } from '@ant-design/icons'
 const Footer = () => {
   return (
-    <footer>
-        App Footer
+    <footer className=' footer'>
+      <div  className='container'>
+      <div>
+        <h3>Lily Shop</h3>
+        <p>Hotline: 0348.863.083-0963.155.397</p>
+        <p>address: 11 /02/03 Luong The Vinh Ha Noi</p>
+      </div>
+      <div></div>
+      <div>
+        <p>Customer satisfaction is our pleasure, which is also the motivation to help the shop grow more and more</p>
+        <span><FacebookOutlined /><InstagramOutlined /></span>
+      </div>
+      </div>
     </footer>
   )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,56 +10,58 @@ const contentStyle = {
 const Home = () => {
   return (
     <>
+      <main className='main-home'>
 
-      <section className='sec1'>
-        <Carousel autoplay>
-          <div>
-            <h3 style={contentStyle}><div className='pic1'>
-              <p className='text-caro myshadow'>Pretty</p>
-              <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
-            </div></h3>
+        <section className='sec1'>
+          <Carousel autoplay>
+            <div>
+              <h3 style={contentStyle}><div className='pic1'>
+                <p className='text-caro myshadow'>Pretty</p>
+                <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
+              </div></h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}><div className='pic2'><p className='text-caro myshadow'>Forever</p>
+
+              </div></h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}><div className='pic3'><p className='text-caro myshadow'>Sophisticated</p>
+                <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
+              </div></h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}><div className='pic4'><p className='text-caro myshadow'>Unique</p>
+                <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
+              </div></h3>
+            </div>
+            <div>
+              <h3 style={contentStyle}><div className='pic5'><p className='text-caro myshadow'>particularistic</p></div></h3>
+            </div>
+          </Carousel>
+
+
+        </section>
+        <section className='sec2 container'>
+          <div className='text2 '>
+            <p className='myshadow2 ' style={{ fontSize: '40px', color: '#ff0597' }}>Flower</p> The flowers are meticulously knit meticulously. You can buy in a bouquet or individual
+
           </div>
-          <div>
-            <h3 style={contentStyle}><div className='pic2'><p className='text-caro myshadow'>Forever</p>
+          <div className="img2">
 
-            </div></h3>
           </div>
-          <div>
-            <h3 style={contentStyle}><div className='pic3'><p className='text-caro myshadow'>Sophisticated</p>
-              <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
-            </div></h3>
+        </section>
+        <section className='sec2 container'>
+          <div className="img3">
+
           </div>
-          <div>
-            <h3 style={contentStyle}><div className='pic4'><p className='text-caro myshadow'>Unique</p>
-              <p className='text-caro2 myshadow'>Each product created from us is an art that is hard to deny with people who love them</p>
-            </div></h3>
+          <div className='text2 '>
+            <p className='myshadow2 ' style={{ fontSize: '40px', color: '#ff0597' }}>Animals</p> made sophisticated, lovely and beautifully suitable for everyone's tastes
+
           </div>
-          <div>
-            <h3 style={contentStyle}><div className='pic5'><p className='text-caro myshadow'>particularistic</p></div></h3>
-          </div>
-        </Carousel>
 
-
-      </section>
-      <section className='sec2 container'>
-        <div className='text2 '>
-          <p className='myshadow2 ' style={{ fontSize: '40px', color: '#ff0597' }}>Flower</p> The flowers are meticulously knit meticulously. You can buy in a bouquet or individual
-
-        </div>
-        <div className="img2">
-
-        </div>
-      </section>
-      <section className='sec2 container'>
-        <div className="img3">
-
-        </div>
-        <div className='text2 '>
-          <p className='myshadow2 ' style={{ fontSize: '40px', color: '#ff0597' }}>Animals</p> made sophisticated, lovely and beautifully suitable for everyone's tastes
-
-        </div>
-
-      </section>
+        </section>
+      </main>
     </>
   )
 }
