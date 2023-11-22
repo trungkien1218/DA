c3dcfd13e047f32cd60ab0b093283c73fe0c73d7
trungkien1218 <136812461+trungkien1218@users.noreply.github.com>
2023-11-22 17:06:41 +0700

    weqweq

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,11 +4,17 @@ const Footer = () => {
   return (
     <footer className='container footer'>
       <div>
-        <h3>Lily Shop</h3>
+        <h3 style={{ paddingBottom: '30px' }}>Lily Shop</h3>
         <p>Hotline: 0348.863.083-0963.155.397</p>
         <p>address: 11 /02/03 Luong The Vinh Ha Noi</p>
       </div>
-      <div></div>
+      <div>
+        <h3 style={{ paddingBottom: '30px' }}>Policy</h3>
+        <p>Book of Books</p>
+        <p>Payment book policy</p>
+        <p>Return book policy</p>
+        <p>Transport book policy</p>
+      </div>
       <div>
         <p>Customer satisfaction is our pleasure, which is also the motivation to help the shop grow more and more</p>
         <span><FacebookOutlined /><InstagramOutlined /></span>
